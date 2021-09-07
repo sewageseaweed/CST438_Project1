88746ef867cec370cfef7c0826a1d0bb6b937ff1
Barbara Kondo <bkondo@csumb.edu>
2021-09-07 11:53:52 -0700

    attempted to make test for populating random quote GUI on startup of QuoteFeedActivity
    also changed BookFeedActivity.java to QuoteFeedActivity.java. wasn't thinking straight when I made the file.

--- app/src/main/java/com/example/cst438_project1/QuoteFeedActivity.java
+++ app/src/main/java/com/example/cst438_project1/QuoteFeedActivity.java
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class BookFeedActivity extends AppCompatActivity {
+public class QuoteFeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
