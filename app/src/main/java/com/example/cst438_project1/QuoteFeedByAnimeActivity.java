8bd038ebc90945bfdbd673ac250e1e66c525d2fd
Barbara Kondo <bkondo@csumb.edu>
2021-09-12 19:55:28 -0700

    added activity change button functionality: should be able to go between 10 random quotes (QuoteFeedActivity) and Quotes by Anime (QuoteFeedByAnimeActivity)

--- app/src/main/java/com/example/cst438_project1/QuoteFeedByAnimeActivity.java
+++ app/src/main/java/com/example/cst438_project1/QuoteFeedByAnimeActivity.java
@@ -8,7 +8,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,12 +34,36 @@ public class QuoteFeedByAnimeActivity extends AppCompatActivity {
         final QuoteAdapter adapter = new QuoteAdapter();
 
         recyclerView.setAdapter(adapter);
+
+        // set anime titles into spinner
+        // set up onSelect for spinner to call getQuotesByAnime(selection)
+
+        final Button btn10rand = findViewById(R.id.quoteFeedByAnime_button_10random);
+
+        btn10rand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextActivity("10rand");
+            }
+        });
     }
 
     public static Intent getIntent(Context context) {
         Intent intent = new Intent(context, QuoteFeedByAnimeActivity.class);
 
         return intent;
+    }
+
+    public void nextActivity(String choice) {
+        Intent intent = new Intent();
+
+        if (choice.equals("10rand")) {
+            intent = QuoteFeedActivity.getIntent(getApplicationContext());
+        } else {
+//            intent = QuoteFeedByCharacterActivity.getIntent(getApplicationContext());
+        }
+
+        startActivity(intent);
     }
 
 //    private List<String> getAnimeTitles() {
