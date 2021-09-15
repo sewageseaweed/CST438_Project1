5a18d46eb0fdb4120a21071fe05a1b88c6347c4d
sewageseaweed <csumagang@csumb.edu>
2021-09-15 00:20:11 -0700

    Fixed button. It was fine until we merged.

--- app/src/main/java/com/example/cst438_project1/MainActivity.java
+++ app/src/main/java/com/example/cst438_project1/MainActivity.java
@@ -3,14 +3,28 @@ package com.example.cst438_project1;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public static final String ACTIVITY_LABEL = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Button testButton = findViewById(R.id.main_button);
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to QuoteFeedActivity
+                nextActivity();
+            }
+        });
     }
 
     public void nextActivity() {
