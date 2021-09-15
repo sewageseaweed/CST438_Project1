1087ce6962ef8fda364c7b71f01d091ab1c31956
Andrew Mei <44917258+AndrewMei99@users.noreply.github.com>
2021-09-15 00:01:05 -0700

    Revert "Book filter"

--- app/src/main/java/com/example/cst438_project1/MainActivity.java
+++ app/src/main/java/com/example/cst438_project1/MainActivity.java
@@ -1,21 +1,14 @@
 package com.example.cst438_project1;
 //testing dao branch
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String ACTIVITY_LABEL = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void nextActivity() {
-        Intent intent = QuoteFeedActivity.getIntent(getApplicationContext());
-
-        startActivity(intent);
     }
 }
